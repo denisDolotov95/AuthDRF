91661ee267bf540110e6fcd4dd953487536f9dcd
fox <gambit0095.mail@gmail.com>
2026-05-21 19:43:31 +0300

    initiate migration my core django

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# entrypoint.sh
-echo "Applying migrations..."
-python3 manage.py migrate
-echo "Migrations applied."
-
-exec "$@"
--- pet_project/app/apps.py
+++ pet_project/app/apps.py
@@ -1,13 +1,20 @@
 import os
+import sys
 
 from django.apps import AppConfig
 from django.contrib.auth import get_user_model
+from django.core.management import call_command
 
 
 class AppConfig(AppConfig):
     name = "pet_project.app"
 
     def ready(self):
+
+        # Проверяем, что приложение запущено командой runserver или gunicorn,
+        # чтобы миграции не запускались при выполнении тестов или других команд
+        if "runserver" in sys.argv or "gunicorn" in sys.argv or "uwsgi" in sys.argv:
+            call_command("migrate", interactive=False)
 
         User = get_user_model()
         username = os.getenv("DJ_ADMIN_USER", "admin")
